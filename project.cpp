fa628a7b2ed522aed5d5d0579a3c3b6b4d86320c
roman-stozhuk <Roman.Stozhuk.ShI.2023@lpnu.ua>
2023-10-06 12:20:29 +0300

    abc

--- project.cpp
+++ project.cpp
@@ -2,6 +2,8 @@
 //using namespace std; то так не робиться :)
 
 int main() {
+    int a = 1;
+    std::cout << a << std::endl;
     std::cout << "Hello team!"<< std::endl;
     return 0;
     //а отак
